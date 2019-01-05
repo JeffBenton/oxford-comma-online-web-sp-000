3b50f8dd70a38066f3a321fe16bb38607d3886a0
Jeff Benton <sikletz@gmail.com>
2019-01-05 23:12:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  case array
-  when array.length == 1
+  case array.length
+  when 1
     "#{array[0]}"
-  when array.length == 2
+  when 2
     "#{array[0]} and #{array[1]}"
   else
     "asdf"
